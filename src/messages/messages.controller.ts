4fcb6c50c442e44b9d60b1676cf1f97f7a2e2a70
Rishabh Sawhney <rishabhsawhney@hotmail.com>
2022-03-10 01:45:41 +0100

    Completed the backend for the messages application

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,17 +1,35 @@
-import { Controller, Get, Post, Body, Param } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, NotFoundException } from '@nestjs/common';
 import { CreateMessageDto } from './dtos/create-message.dto';
+import { MessagesService } from './messages.service';
 @Controller('messages')
 export class MessagesController {
-    @Get()
-    listMessages(){}    
 
-    @Post()
-    postMessage(@Body() body:CreateMessageDto){
+    
+
+constructor(public messagesService : MessagesService){
+    
+}
+
+@Get()
+listMessages(){
+   return this.messagesService.findAll();
+}    
+
+@Post()
+postMessage(@Body() body:CreateMessageDto){
     console.log(body);
+    return this.messagesService.create(body.content);
     }
 
-    @Get('/:id')
-    getMessage(@Param('id') id:string){
+@Get('/:id')
+async getMessage(@Param('id') id:string){
     console.log(id);
+    const message = await this.messagesService.findOne(id);
+
+    if(!message){
+        throw new NotFoundException('This is not a valid id');
     }
+    return message;
+    }
+
 }
